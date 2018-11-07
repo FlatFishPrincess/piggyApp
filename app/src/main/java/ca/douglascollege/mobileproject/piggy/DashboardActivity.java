3c865659c54ad9e7c04cdb6b768523ed9bed59da
Jiweon Park <jiweon.park@gracehanin.org>
2018-11-07 05:50:20 -0800

    added fragement

--- app/src/main/java/ca/douglascollege/mobileproject/piggy/DashboardActivity.java
+++ app/src/main/java/ca/douglascollege/mobileproject/piggy/DashboardActivity.java
@@ -1,10 +1,22 @@
 package ca.douglascollege.mobileproject.piggy;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Fade;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
+    private FrameLayout frameLayout;
+    private DashboardFragment dashboardFragment;
+    private LogoutFragment logoutFragment;
+    private ReportFragment reportFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,49 @@ public class DashboardActivity extends AppCompatActivity {
         Fade fade = new Fade();
         getWindow().setEnterTransition(fade);
         getWindow().setExitTransition(fade);
+
+        frameLayout = findViewById(R.id.dashboard_frame);
+        bottomNav = findViewById(R.id.bottom_nav);
+
+        dashboardFragment = new DashboardFragment();
+        logoutFragment = new LogoutFragment();
+        reportFragment = new ReportFragment();
+
+        //setting default fragment layout
+        setFrame(dashboardFragment);
+
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                return false;
+                switch (menuItem.getItemId()){
+                    case R.id.home: {
+//                        bottomNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFrame(dashboardFragment);
+                        return true;
+                    }
+                    case R.id.report: {
+//                        bottomNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFrame(reportFragment);
+                        return true;
+                    }
+                    case R.id.logout: {
+//                        bottomNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFrame(logoutFragment);
+                        return true;
+                    }
+                    default:
+                        return false;
+                }
+            }
+        });
+
+    }
+
+    private void setFrame(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dashboard_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
